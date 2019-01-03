1f069b28d91741ce20afdb0c5ccc88e1e2460327
Sicphy <petrov.kirill.1998@mail.ru>
2019-01-03 21:54:55 +0300

    added some tests

--- Code/genres/tests.py
+++ Code/genres/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from genres.models import Genre
+from goods.models import Good
 
 # Create your tests here.
+
+class GenreClassTest(TestCase):
+    
+    def create_genre(self, name="Adventure"):
+        return Genre.objects.create(name=name)
+
+    def test_genre_creation(self):
+        g = self.create_genre()
+        self.assertTrue(isinstance(g, Genre))
+
+    
--- Code/main/tests.py
+++ Code/main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class MainClassTest(TestCase):
+    
+    def test_main_view(self):
+        url = reverse("main")
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+    
